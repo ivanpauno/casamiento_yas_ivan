3969b325a382883f789e75d3e9def5aadd09679e
Ivan Santiago Paunovic <ivanpauno@gmail.com>
2026-06-30 21:20:27 -0300

    try to fix deployment
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-   devToolbar: {
+  site: 'https://ivanpauno.github.io/casamiento_yas_ivan/',
+  base: '/casamiento_yas_ivan/',
+  devToolbar: {
     enabled: false
   }
 });
