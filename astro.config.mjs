88c092a7748ce12975ca1c072671fb1e1a36037e
Ivan Santiago Paunovic <ivanpauno@gmail.com>
2026-07-10 20:02:17 -0300

    remove base now that we have domain
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ivanpauno.github.io/casamiento_yas_ivan/',
-  base: '/casamiento_yas_ivan/',
+  // base: '/casamiento_yas_ivan/',
   devToolbar: {
     enabled: false
   }
